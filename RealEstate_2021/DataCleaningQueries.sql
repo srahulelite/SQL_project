da23b5c8f789c1c6ca4d6cabcebfbb789c2d3bf0
Shukla <s.rahulelite@gmail.com>
2022-10-08 02:23:55 +0530

    Populate Missing data in PropertyAddress

--- RealEstate_2021/DataCleaningQueries.sql
+++ RealEstate_2021/DataCleaningQueries.sql
@@ -1,6 +1,7 @@
 -- Select top 100 rows to visualize the data
 select top 100 * from RealEState Limit
 
+
 -- Reformat SaleDate Column to Standardize Date
 select saledate from RealEState;
 
@@ -11,4 +12,20 @@ update RealEState
 Set SaleDateConverted = Convert(Date, SaleDate)
 
 select saledate, SaleDateConverted from RealEState;
+
+
+--Populate Missing data in PropertyAddress
+select a.UniqueID, b.[UniqueID ], a.parcelID, a.PropertyAddress, b.parcelID, b.PropertyAddress 
+from RealEState a JOIN RealEState b on
+a.ParcelID = b.ParcelID and
+a.[UniqueID ] <> b.[UniqueID ]
+--where a.PropertyAddress is null
+
+update a
+set PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+from RealEState a
+join RealEState b on
+a.ParcelID = b.ParcelID and
+a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is null
 
