862d681326647be6ab4afe1f258da1ffbde277ec
Shukla <s.rahulelite@gmail.com>
2022-10-08 03:01:01 +0530

    Breaking down addresses by substr and parsing Done

--- RealEstate_2021/DataCleaningQueries.sql
+++ RealEstate_2021/DataCleaningQueries.sql
@@ -14,7 +14,7 @@ Set SaleDateConverted = Convert(Date, SaleDate)
 select saledate, SaleDateConverted from RealEState;
 
 
---Populate Missing data in PropertyAddress
+-- Populate Missing data in PropertyAddress
 select a.UniqueID, b.[UniqueID ], a.parcelID, a.PropertyAddress, b.parcelID, b.PropertyAddress 
 from RealEState a JOIN RealEState b on
 a.ParcelID = b.ParcelID and
@@ -29,3 +29,60 @@ a.ParcelID = b.ParcelID and
 a.[UniqueID ] <> b.[UniqueID ]
 where a.PropertyAddress is null
 
+
+-- Breaking Property Address into different Columns
+select PropertyAddress from RealEState
+
+select PropertyAddress, 
+Substring(PropertyAddress, 1, (CHARINDEX(',', PropertyAddress)-1))
+as address1,
+Substring(PropertyAddress, (CHARINDEX(',',PropertyAddress)+2), (CHARINDEX(',', PropertyAddress)-1))
+as  address2
+from RealEState
+
+
+select Substring(PropertyAddress, (CHARINDEX(',',PropertyAddress)+2), (CHARINDEX(',', PropertyAddress)-1))
+as address from RealEState
+
+Alter table RealEstate
+Add PropertySplitAddress nvarchar(255)
+
+Alter table RealEstate
+Add PropertySplitCity nvarchar(255)
+
+update RealEState
+set PropertySplitAddress = Substring(PropertyAddress, 1, (CHARINDEX(',', PropertyAddress)-1))
+
+update RealEState
+set PropertySplitCity = Substring(PropertyAddress, (CHARINDEX(',',PropertyAddress)+2), (CHARINDEX(',', PropertyAddress)-1))
+
+select * from RealEState
+
+-- Breaking down OwnerAddress in Columns
+select 
+PARSENAME(replace(OwnerAddress, ',', '.'), 3),
+PARSENAME(replace(OwnerAddress, ',', '.'), 2),
+PARSENAME(replace(OwnerAddress, ',', '.'), 1)
+from RealEState
+
+Alter table RealEstate
+Add OwnerSplitAddress1 nvarchar(255)
+
+Alter table RealEstate
+Add OwnerSplitAddress2 nvarchar(255)
+
+Alter table RealEstate
+Add OwnerSplitAddress3 nvarchar(255)
+
+update RealEState
+set OwnerSplitAddress1 = PARSENAME(replace(OwnerAddress, ',', '.'), 3)
+
+
+update RealEState
+set OwnerSplitAddress2 = PARSENAME(replace(OwnerAddress, ',', '.'), 2)
+
+
+update RealEState
+set OwnerSplitAddress3 = PARSENAME(replace(OwnerAddress, ',', '.'), 1)
+
+select * from RealEState
